09f910a92851aaba466490db7b64a5e1b9ff9850
henry-prince <henry.prince@student.hhs.school.nz>
2021-05-24 10:06:08 +1200

    firt task

---
+++ hello world.py
@@ -0,0 +1,15 @@
+print ("hello world\n")
+hello = ("hello world\n")
+print(hello * 5)
+
+print("who is candice? ")
+answer1 = ""
+
+
+while answer1 != "idk":
+
+    answer1 = input()
+    if answer1 == "idk":
+            print("candice dick fit in yo mouth")
+    else:
+            print("say idk")
